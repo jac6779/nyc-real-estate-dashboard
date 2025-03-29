dad5780fd664406bc08502d309a81c1724207c7c
jac6779 <144076868+jac6779@users.noreply.github.com>
2025-03-29 11:56:49 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const url = "https://public.tableau.com/shared/5JX7SQJ2T?:display_count=n&:origi
 const vizContainer = document.getElementById('vizContainer');
 const options = {
     hideTabs: true,
-    height: 1000,
-    width: 1200,
+    height: 1520,
+    width: 760,
     onFirstInteraction: function() {
         workbook = viz.getWorkbook();
         activeSheet = workbook.getActiveSheet();
